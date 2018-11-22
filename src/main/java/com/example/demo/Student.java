bae74858678fda8eec621885c4917153b756c817
Isabel Navarro-Laurent <isabelnavarro-laurent@Isabels-MacBook-Air.local>
2018-11-21 21:11:12 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String realname;
+
+    @ManyToMany(mappedBy = "cast")
+    private Set<Course> courses;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public Set<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
+    }
+}
